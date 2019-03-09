d97bd3a3c9ad229b3bfa38a408d014e09982911b
Isaiah Mann <isaiah@truex.com>
2019-03-08 17:04:44 -0800

    Fix curl command formatting

--- release_pod.sh
+++ release_pod.sh
@@ -6,11 +6,10 @@ repo_owner=$2
 repo_name=$3
 repo_token=$4
 pod_version=$(grep s.version $pod_name.podspec -m 1 | grep -o '"[^"]\+"'  | tr -d "\"")
-echo $pod_version
-curl --data "{'tag_name': 'v${pod_version}',
-              'target_commitish': 'master',
-              'name': 'v${pod_version}',
-              'body': 'Release of version ${pod_version}',
-              'draft': false,
-              'prerelease': false}"
-      https://api.github.com/repos/${repo_owner}/${repo_name}/releases?access_token=${repo_token}
+request_body=$(echo "{'tag_name': 'v${pod_version}',
+                      'target_commitish': 'master',
+                      'name': 'v${pod_version}',
+                      'body': 'Release of version ${pod_version}',
+                      'draft': false,
+                      'prerelease': false}" | sed "s/'/\"/g")
+curl --data $request_body https://api.github.com/repos/${repo_owner}/${repo_name}/releases?access_token=${repo_token}
