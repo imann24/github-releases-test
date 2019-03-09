d0e4fce2b8aec71c7e8b45c31b58c2a73408adf4
Isaiah Mann <isaiah@truex.com>
2019-03-08 16:49:25 -0800

    Add podspec and script to generate release version

---
+++ TestPod.podspec
@@ -0,0 +1,4 @@
+Pod::Spec.new do |s|
+  s.name         = "testPod"
+  s.version      = "1.3"
+end
---
+++ release_pod.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# run at the base of your spec repo
+# usage ./release_pod.sh <pod name> <repo owner> <repo name> <token>
+pod_name=$1
+repo_owner=$2
+repo_name=$3
+repo_token=$4
+pod_version=$(grep s.version $pod_name.podspec -m 1 | grep -o '"[^"]\+"'  | tr -d "\"")
+echo $pod_version
+curl --data "{'tag_name': 'v${pod_version}',
+              'target_commitish': 'master',
+              'name': 'v${pod_version}',
+              'body': 'Release of version ${pod_version}',
+              'draft': false,
+              'prerelease': false}"
+      https://api.github.com/repos/${repo_owner}/${repo_name}/releases?access_token=${repo_token}
